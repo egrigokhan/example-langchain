aa9bcdc4f2f1ea1865314d5baa6968f5d0dd2587
Gokhan <egrigokhan@gmail.com>
2023-03-13 18:35:28 -0400

    changed port

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from src.index import query
 
 app = Flask(__name__)
 
+
 @app.route('/query', methods=['POST'])
 def handle_query():
     message = request.json['msg']
     response = query(message)
     return jsonify(response)
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8080)
